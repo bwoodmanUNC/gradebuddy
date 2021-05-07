05741384eda806a6e7a59d6181deb0e318a5ef79
Benjamin Woodman <benwoodman101@gmail.com>
2021-05-07 19:13:16 -0400

    added configs

--- main.py
+++ main.py
@@ -16,10 +16,23 @@ import boto3
 
 import uuid
 
-SALT = b'\xe0\xfd\xdb4\x07<\xb5\xcd\x97\xa1]\x94yo>\x07~\x13\xc0\xeb\x13^\x1f$ \xf1<Xx"\x19\x0f'
-SECRET_KEY = "89a571ae0f0895c88b639ad25c2abdb74716cbc2e92ebde082635ba0f76dace2"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 100
+import os
+
+# SALT = b'\xe0\xfd\xdb4\x07<\xb5\xcd\x97\xa1]\x94yo>\x07~\x13\xc0\xeb\x13^\x1f$ \xf1<Xx"\x19\x0f'
+# SECRET_KEY = "89a571ae0f0895c88b639ad25c2abdb74716cbc2e92ebde082635ba0f76dace2"
+# ALGORITHM = "HS256"
+# ACCESS_TOKEN_EXPIRE_MINUTES = 100
+
+SALT = os.environ['SALT']
+SECRET_KEY = os.environ['SECRET_KEY']
+ALGORITHM = os.environ['ALGORITHM']
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ['ACCESS_TOKEN_EXPIRE_MINUTES'])
+
+DATABASE_ADDRESS = os.environ['DATABASE_ADDRESS']
+DATABASE_USER = os.environ['DATABASE_USER']
+DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
+
+
 
 def create_connection(host, user, password, database, port='3306'):
     connection = None
@@ -58,7 +71,7 @@ def get_password_hash(plain_password):
 # @app.get('/login')
 def get_user(username):
     # username = 'babin101'
-    connection = create_connection('143.198.112.73', 'ben3', 'P1neappleRunn3rd', 'grading')
+    connection = create_connection(DATABASE_ADDRESS, DATABASE_USER, DATABASE_PASSWORD, 'grading')
     # query = 'SELECT * FROM "grading.users" WHERE username="' + username + '"'
     user_table = Table('users')
     query = str(Query.from_(user_table).select('*').where(user_table.username == username))
@@ -161,7 +174,7 @@ def verify_class_student(connection, current_user, indv_class_id):
 
 @app.get("/list/classes")
 async def list_all_user_classes(current_user: User = Depends(get_current_user)):
-    connection = create_connection('143.198.112.73', 'ben3', 'P1neappleRunn3rd', 'grading')
+    connection = create_connection(DATABASE_ADDRESS, DATABASE_USER, DATABASE_PASSWORD, 'grading')
     tbl_students = Table('students')
     tbl_classes = Table('classes')
     query = str(Query.from_(tbl_students).join(tbl_classes).on(tbl_students.class_id == tbl_classes.id).select(tbl_classes.id, tbl_classes.name).where(tbl_students.student_id == current_user.id))
@@ -169,12 +182,12 @@ async def list_all_user_classes(current_user: User = Depends(get_current_user)):
     cursor = connection.cursor()
     cursor.execute(query)
     class_list = [IndvClass(id=r[0], name=r[1]) for r in cursor.fetchall()]
-
+    connection.close()
     return class_list
 
 @app.get("/list/owned_classes")
 async def list_all_user_classes(current_user: User = Depends(get_current_user)):
-    connection = create_connection('143.198.112.73', 'ben3', 'P1neappleRunn3rd', 'grading')
+    connection = create_connection(DATABASE_ADDRESS, DATABASE_USER, DATABASE_PASSWORD, 'grading')
     tbl_classes = Table('classes')
     query = str(Query.from_(tbl_classes).select(tbl_classes.id, tbl_classes.name).where(tbl_classes.user == current_user.id))
     print(query)
@@ -182,11 +195,12 @@ async def list_all_user_classes(current_user: User = Depends(get_current_user)):
     cursor.execute(query)
     class_list = [IndvClass(id=r[0], name=r[1]) for r in cursor.fetchall()]
 
+    connection.close()
     return class_list
 
 @app.get("/class/{class_id}")
 async def get_class_assignments(class_id: int, current_user: User = Depends(get_current_user)):
-    connection = create_connection('143.198.112.73', 'ben3', 'P1neappleRunn3rd', 'grading')
+    connection = create_connection(DATABASE_ADDRESS, DATABASE_USER, DATABASE_PASSWORD, 'grading')
     tbl = Table('assignments')
     query = str(Query.from_(tbl).select('*').where(tbl.class_id == class_id))
     print(query)
@@ -194,11 +208,12 @@ async def get_class_assignments(class_id: int, current_user: User = Depends(get_
     cursor.execute(query)
     rows = cursor.fetchall()
     assignments = [Assignment(id=r[0], name=r[1], class_id=r[2], num_uploads=r[3]) for r in rows]
+    connection.close()
     return assignments
 
 @app.get("/assignments/{assignment_id}")
 async def get_assignment_uploads(assignment_id: int, current_user: User = Depends(get_current_user)):
-    connection = create_connection('143.198.112.73', 'ben3', 'P1neappleRunn3rd', 'grading')
+    connection = create_connection(DATABASE_ADDRESS, DATABASE_USER, DATABASE_PASSWORD, 'grading')
     tbl = Table('uploads')
     tbl_users = Table('users')
     query = str(Query.from_(tbl).select('*').where(tbl.assignment_id == assignment_id).join(tbl_users).on(tbl.user_id == tbl_users.id))
@@ -208,11 +223,12 @@ async def get_assignment_uploads(assignment_id: int, current_user: User = Depend
     rows = cursor.fetchall()
     uploads = [IndvUpload(id=r[0], assignment_id=r[1], link=r[2], user_id=r[3], user_name=r[6]) for r in rows]
     # print(rows)
+    connection.close()
     return uploads
 
 @app.get('/uploads/{upload_id}')
 async def get_upload_information(upload_id: int, current_user: User = Depends(get_current_user)):
-    connection = create_connection('143.198.112.73', 'ben3', 'P1neappleRunn3rd', 'grading')
+    connection = create_connection(DATABASE_ADDRESS, DATABASE_USER, DATABASE_PASSWORD, 'grading')
     tbl_uploads = Table('uploads')
     tbl_grades = Table('grades')
     tbl_assignments = Table('assignments')
@@ -246,7 +262,7 @@ async def get_upload_information(upload_id: int, current_user: User = Depends(ge
 @app.post("/new/grade")
 async def add_grade_for_upload(upload_id: int = Form(...), score: float = Form(...), selected_rubric_items: str = Form(...), current_user: User = Depends(get_current_user)):
     try:
-        connection = create_connection('143.198.112.73', 'ben3', 'P1neappleRunn3rd', 'grading')
+        connection = create_connection(DATABASE_ADDRESS, DATABASE_USER, DATABASE_PASSWORD, 'grading')
         tbl_uploads = Table('uploads')
         tbl_assignments = Table('assignments')
         cursor = connection.cursor()
@@ -265,8 +281,10 @@ async def add_grade_for_upload(upload_id: int = Form(...), score: float = Form(.
             print(query)
             cursor.execute(query)
             connection.commit()
+            connection.close()
             return True
         else:
+            connection.close()
             raise HTTPException(401)
     except Error as e:
         print(e)
@@ -275,7 +293,7 @@ async def add_grade_for_upload(upload_id: int = Form(...), score: float = Form(.
 @app.put("/update/grade")
 async def add_existing_grade_for_upload(grade_id: Optional[int] = Form(...), upload_id: int = Form(...), score: float = Form(...), selected_rubric_items: str = Form(...), current_user: User = Depends(get_current_user)):
     try:
-        connection = create_connection('143.198.112.73', 'ben3', 'P1neappleRunn3rd', 'grading')
+        connection = create_connection(DATABASE_ADDRESS, DATABASE_USER, DATABASE_PASSWORD, 'grading')
         tbl_uploads = Table('uploads')
         tbl_assignments = Table('assignments')
         cursor = connection.cursor()
@@ -294,8 +312,10 @@ async def add_existing_grade_for_upload(grade_id: Optional[int] = Form(...), upl
             print(query)
             cursor.execute(query)
             connection.commit()
+            connection.close()
             return True
         else:
+            connection.close()
             raise HTTPException(401)
     except Error as e:
         print(e)
@@ -305,7 +325,7 @@ async def add_existing_grade_for_upload(grade_id: Optional[int] = Form(...), upl
 @app.post("/new/upload")
 async def add_upload_for_assignment(new_upload: UploadFile = File(...), assignment_id: int = Form(...), current_user: User = Depends(get_current_user)):
     try:
-        connection = create_connection('143.198.112.73', 'ben3', 'P1neappleRunn3rd', 'grading')
+        connection = create_connection(DATABASE_ADDRESS, DATABASE_USER, DATABASE_PASSWORD, 'grading')
         tbl = Table('assignments')
         query = str(Query.from_(tbl).select('*').where(tbl.id == assignment_id))
         print(query)
@@ -352,7 +372,7 @@ async def add_upload_for_assignment(new_upload: UploadFile = File(...), assignme
 @app.post("/new/assignment")
 async def add_assignment_endpoint(new_assignment: Assignment, current_user: User = Depends((get_current_user))):
     try:
-        connection = create_connection('143.198.112.73', 'ben3', 'P1neappleRunn3rd', 'grading')
+        connection = create_connection(DATABASE_ADDRESS, DATABASE_USER, DATABASE_PASSWORD, 'grading')
         if verify_class_user(connection, current_user, new_assignment.class_id):
             tbl = Table('assignments')
             query = str(Query.into(tbl).columns(tbl.name, tbl.class_id, tbl.num_uploads, tbl.rubric).insert(new_assignment.name, new_assignment.class_id, 0, new_assignment.rubric))
@@ -364,7 +384,7 @@ async def add_assignment_endpoint(new_assignment: Assignment, current_user: User
             cursor.execute(query_get_inserted_val)
             print('asdf')
             resp = cursor.fetchall()
-            
+            connection.close()
             return resp[0][0]
         else:
             connection.close()
@@ -378,7 +398,7 @@ async def add_assignment_endpoint(new_assignment: Assignment, current_user: User
 async def add_class_endpoint(new_class: IndvClass, current_user: User = Depends((get_current_user))):
     if current_user.is_power:
         new_class.user = current_user.id
-        connection = create_connection('143.198.112.73', 'ben3', 'P1neappleRunn3rd', 'grading')
+        connection = create_connection(DATABASE_ADDRESS, DATABASE_USER, DATABASE_PASSWORD, 'grading')
         classes_table = Table('classes')
         query = str(Query.into(classes_table).columns(classes_table.name, classes_table.user).insert(new_class.name, new_class.user))
         print(query)
